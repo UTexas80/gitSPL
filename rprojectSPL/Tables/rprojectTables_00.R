acef3629a3c5a189296fe6ea1c0311e0b0db611a
UTexas80 <glen.falk@utexas.edu>
2017-11-27 15:47:15 -0500

    20171127SPL
    Updated usual suspects

--- rprojectSPL/Tables/rprojectTables_00.R
+++ rprojectSPL/Tables/rprojectTables_00.R
@@ -1,11 +1,13 @@
 library(tidyquant)                                                              # Loads tidyquant, tidyverse, lubridate, quantmod, TTR, and xts
 library(TTR)                                                                    # Technical Trading Rules package
-# SPL Processing ---------------------------------------------------------------
+# SPL Processing -----------------------------------------------------------------------------------
 SPL <- tq_get("SPL.AX")                                                         # Get SPL Stock Prices
 SPL<-SPL[complete.cases(SPL),]                                                  # Delete NA
-date <- SPL$date                                                                # Create date variable
+date-1 <- SPL$date                                                                # Create date variable
+date<-date-1 + 1
 key<-paste(as.character(format(date, "%Y")), as.character(format(date, "%m")), as.character(format(date, "%d")), "spl", sep = "")
-# TTR Processing --------------------------------------------------------------- 
+# key<-paste(as.character(format(date, "%Y")), as.character(format(date, "%m")), as.character(format(date, "%d")), "spl", sep = "")
+# TTR Processing ----------------------------------------------------------------------------------- 
 ad <- chaikinAD(SPL[,c("high","low","close")], SPL[,"volume"])                  # Chaikin Accumulation / Distribution Index
 adx <- ADX(SPL[,c("high","low","close")])                                       # Welles Wilder’s Directional Movement Index
 aroon <- aroon(SPL[,c("high","low")],n=20)                                      # Aroon Indicator
@@ -61,7 +63,7 @@ vwas.20 <- VWAP(SPL$close, SPL$volume, 20)                                      
 wma.20 <- WMA(SPL$close, 20)                                                    # Weighted Moving Average - MA is similar to an EMA, but with linear weighting if the length of wts is equal to n. If the length of wts is equal to the length of x, the WMA will use the values of wts as weights.       
 zz <- ZigZag( SPL[,c("high", "low")], change=20 )                               # Zig Zag higlights trends by removing price changes smaller than change and interpolating lines between the extreme points.
 zlema.20 <- ZLEMA(SPL$close, 20)                                                # ZLEMA - 20 Day; ZLEMA is similar to an EMA, as it gives more weight to recent observations, but attempts to remove lag by subtracting data prior to (n-1)/2 periods (default) to minimize the cumulative effect.
-# Create Table -----------------------------------------------------------------
+# Create Table -------------------------------------------------------------------------------------
 # tblEMV <- data.frame(key,date,"spl",emv)                                          # Create Arms’ Ease of Movement Table
 # Momentum Tables
 tblMOM <- data.frame(key,date,"spl",mom)                                            # Create Momentum Table
@@ -72,13 +74,13 @@ tblMOM_DPOvolume <- data.frame(key,date,"spl",momDPOvolume)                     
 tblMOM_ROC <- data.frame(key,date,"spl",roc)                                        # Create Rate of Change Table
 tblMOM_RSI <- data.frame(key,date,"spl",rsi)                                        # Create Relative Strength Indicator Table
 tblMOM_Stoch <- data.frame(key,date,"spl",stoch)                                    # Create Stochastic Oscillator / Stochastic Momentum Index Table
-tblMOM_UltOsc <-data.frame(date,"spl",ult.osc)                                  # Create Ultimate Oscillator Table
+tblMOM_UltOsc <-data.frame(key, date,"spl",ult.osc)                                 # Create Ultimate Oscillator Table
 tblMOM_WAD <- data.frame(key,date,"spl",wad)                                        # Create Williams Accumulation / Distribution Table
 tblMOM_WPR <- data.frame(key,date,"spl",wpr)                                        # Create William's %R Indicator Table
 # Money Tables
 tblMoneyMFI <- data.frame(key,date,"spl",mfi)                                       # Create Money Flow Index Table
 # Price Tables
-tblPrice <- data.frame(key,SPL,"spl", date + 1)                                     # Create Stock Price Table
+tblPrice <- data.frame(key,SPL,"spl", date-1)                                     # Create Stock Price Table
 #Statistical Tables
 tblStatRPR <- data.frame(key,date,"spl",rpr)                                        # Create Run Percent Rank Table
 tblStatRunSD <- data.frame(key,date,"spl",SPL$close, runsd)                         # Create Running Standard Deviation Table
@@ -106,7 +108,7 @@ tblVolumeChaikinAD <- data.frame(key,date,"spl",ad)                             
 tblVolumeChaikinMF <- data.frame(key,date,"spl",cmf)                                # Create Chaikan Money Flow A/D Table
 tblVolumeOBV <- data.frame(key,date,"spl",obv)                                      # Create OBV Table
 # Moving Average Tables
-tblMA_ALMA20 <- data.frame(key,"spl", alma.20)                                      # Create Arnaud Legoux moving average table
+tblMA_ALMA20 <- data.frame("spl", alma.20)                                      # Create Arnaud Legoux moving average table
 tblMA_DEMA20 <- data.frame(key,date,"spl", dema.20)                                 # Create Double Exponential Moving Average table
 tblMA_EMA20 <- data.frame(key,date,"spl", ema.20)                                   # Create Exponential Moving Average table
 tblMA_EVWMA20 <- data.frame(key,date,"spl", evwma.20)                               # Create # Elastic Volume-weighted Moving Average table
@@ -117,7 +119,7 @@ tblMA_TRIX <- data.frame(key,date,"spl",trix)                                   
 tblMA_VWAS20 <- data.frame(key,date,"spl", vwas.20)                                 # Create Volume-weighted moving average table
 tblMA_WMA20 <- data.frame(key,date,"spl", wma.20)                                   # Create # Weighted Moving Average table
 tblMA_ZLEMA20 <- data.frame(key,date,"spl", zlema.20)                               # Create ZLEMA Table
-# Delete Table NA --------------------------------------------------------------
+# Delete Table NA ----------------------------------------------------------------------------------
 # tblEMV<-tblEMV[complete.cases(tblEMV),]                                       # Delete tblEMV NA
 # Momentum Tables
 tblMOM<-tblMOM[complete.cases(tblMOM),]                                         # Delete tblATR14 NA's
@@ -174,74 +176,74 @@ tblMA_TRIX <- tblMA_TRIX[complete.cases(tblMA_TRIX),]                           
 tblMA_VWAS20 <- tblMA_VWAS20[complete.cases(tblMA_VWAS20),]                     # Delete Volume-weighted moving average NA
 tblMA_WMA20 <- tblMA_WMA20[complete.cases(tblMA_WMA20),]                        # Delete # Weighted Moving Average NA
 tblMA_ZLEMA20<-tblMA_ZLEMA20[complete.cases(tblMA_ZLEMA20),]                    # Delete ZLEMA NA
-# Rename Table columns ---------------------------------------------------------
-# colnames(tblEMV) <- c("date", "symbol", "emv", "emvMA")                       # Rename tblEMV Table Columns
+# Rename Table columns -----------------------------------------------------------------------------
+# colnames(tblEMV) <- c("key","date", "symbol", "emv", "emvMA")                       # Rename tblEMV Table Columns
 
 # Momentum Tables
-colnames(tblMOM) <- c("date", "symbol", "mom")                                  # Rename tblMOM Table Columns
-colnames(tblMOM_CCI) <- c("date", "symbol", "cci" )                             # Rename tblMOM_CCI Table Columns
-colnames(tblMOM_CMO) <- c("date", "symbol", "cmo" )                             # Rename tblMOM_CMO Table Columns
-colnames(tblMOM_DPOprice) <- c("date", "symbol", "DPOprice" )                   # Rename tblMOM_DPOprice Table Columns
-colnames(tblMOM_DPOvolume) <- c("date", "symbol", "DPOvolume" )                 # Rename tblMOM_DPOprice Table Columns
-colnames(tblMOM_ROC) <- c("date", "symbol", "roc" )                             # Rename tblMOM_ROC Table Columns
-colnames(tblMOM_RSI) <- c("date", "symbol", "rsi" )                             # Rename tblMOM_RSI Table Columns
-colnames(tblMOM_Stoch) <- c("date", "symbol", "fastK", "fastD", "slowD")        # Rename tblMOM_Stoch Table Columns
-colnames(tblMOM_UltOsc) <- c("date", "symbol", "ultOsc")                        # Rename tblMOM_UltOsc Table Columns  
-colnames(tblMOM_WAD) <- c("date", "symbol", "williamsAD" )                      # Rename tblWPR Table Columns
-colnames(tblMOM_WPR) <- c("date", "symbol", "wpr" )                             # Rename tblMOM_WPR Table Columns
+colnames(tblMOM) <- c("key","date", "symbol", "mom")                                  # Rename tblMOM Table Columns
+colnames(tblMOM_CCI) <- c("key","date", "symbol", "cci" )                             # Rename tblMOM_CCI Table Columns
+colnames(tblMOM_CMO) <- c("key","date", "symbol", "cmo" )                             # Rename tblMOM_CMO Table Columns
+colnames(tblMOM_DPOprice) <- c("key","date", "symbol", "DPOprice" )                   # Rename tblMOM_DPOprice Table Columns
+colnames(tblMOM_DPOvolume) <- c("key","date", "symbol", "DPOvolume" )                 # Rename tblMOM_DPOprice Table Columns
+colnames(tblMOM_ROC) <- c("key","date", "symbol", "roc" )                             # Rename tblMOM_ROC Table Columns
+colnames(tblMOM_RSI) <- c("key","date", "symbol", "rsi" )                             # Rename tblMOM_RSI Table Columns
+colnames(tblMOM_Stoch) <- c("key","date", "symbol", "fastK", "fastD", "slowD")        # Rename tblMOM_Stoch Table Columns
+colnames(tblMOM_UltOsc) <- c("key","date", "symbol", "ultOsc")                      # Rename tblMOM_UltOsc Table Columns  
+colnames(tblMOM_WAD) <- c("key","date", "symbol", "williamsAD" )                      # Rename tblWPR Table Columns
+colnames(tblMOM_WPR) <- c("key","date", "symbol", "wpr" )                             # Rename tblMOM_WPR Table Columns
 # Money
-colnames(tblMoneyMFI) <- c("date", "symbol", "mfi" )                            # Rename tblMoneyMFI Table Columns
+colnames(tblMoneyMFI) <- c("key","date", "symbol", "mfi" )                            # Rename tblMoneyMFI Table Columns
 # Price
-colnames(tblPrice) <- c("date-1", "open", "high", "low", "close", "volume", 
-  "adjusted", "symbol", "date" )                                                # Rename tblPBands Table Columns
+colnames(tblPrice) <- c("key","date", "open", "high", "low", "close", "volume", 
+  "adjusted", "symbol", "date-1" )                                                # Rename tblPBands Table Columns
 #Statistical Tables
-colnames(tblStatRPR) <- c("date", "symbol", "rpr" )                             # Rename tblRPR Table Columns
-colnames(tblStatRunSD) <- c("date", "symbol", "close", "runsd" )                # Rename tblRunSD Table Columns
+colnames(tblStatRPR) <- c("key","date", "symbol", "rpr" )                             # Rename tblRPR Table Columns
+colnames(tblStatRunSD) <- c("key","date", "symbol", "close", "runsd" )                # Rename tblRunSD Table Columns
 # Trend Tables
-colnames(tblTrendADX) <- c("date", "symbol", "DIp", "DIn","DX","ADX")           # Rename tblTrendADX Table Columns
-colnames(tblTrendAROON) <- c("date", "symbol", "aroonUp", "aroonDn", "oscillator"  ) # Rename tblTrendAROON Table Columns
-colnames(tblTrendCLV) <- c("date", "symbol", "clv" )                            # Rename tblTrendCLV Table Columns
-colnames(tblTrendDC) <- c("date", "symbol", "high", "mid", "low" )              # Rename tblTrendDC Table Columns
-colnames(tblTrendDVI) <- c("date", "symbol", "dviMag", "dviStr", "dvi")         # Rename tblTrendDVI Table Columns
-colnames(tblTrendGMMA) <- c("date", "symbol", "short.lag.3", "short.lag.5", 
+colnames(tblTrendADX) <- c("key","date", "symbol", "DIp", "DIn","DX","ADX")           # Rename tblTrendADX Table Columns
+colnames(tblTrendAROON) <- c("key","date", "symbol", "aroonUp", "aroonDn", "oscillator"  ) # Rename tblTrendAROON Table Columns
+colnames(tblTrendCLV) <- c("key","date", "symbol", "clv" )                            # Rename tblTrendCLV Table Columns
+colnames(tblTrendDC) <- c("key","date", "symbol", "high", "mid", "low" )              # Rename tblTrendDC Table Columns
+colnames(tblTrendDVI) <- c("key","date", "symbol", "dviMag", "dviStr", "dvi")         # Rename tblTrendDVI Table Columns
+colnames(tblTrendGMMA) <- c("key","date", "symbol", "short.lag.3", "short.lag.5", 
   "short.lag.8", "short.lag.10", "short.lag.12", "short.lag.15", "long.lag.30", 
   "long.lag.35", "long.lag.40", "long.lag.45", "long.lag.50", "long.lag.60"  )  # Rename tblTrendGMMA Table Columns
-colnames(tblTrendMACD) <- c("date", "symbol","macd", "signal"  )                # Rename tblTrendMACD Table Columns
-colnames(tblTrendTDI) <- c("date", "symbol", "tdi", "di" )                      # Rename tblTrendTCI Table Columns
-colnames(tblTrendVHF) <- c("date", "symbol", "vhfClose")                        # Rename tblTrendTCI Table Columns
-colnames(tblTrendVHF_HiLo) <- c("date", "symbol", "vhfHiLo")                    # Rename tblTrendTCI Table Columns
-colnames(tblTrendWilderSum) <- c("date", "symbol", "runWilderSum")              # Rename tblTrendWilderSum Table Columns
-colnames(tblTrendZigZag) <- c("date", "symbol", "zigzag")                       # Rename tblTrendZigZag Table Columns
+colnames(tblTrendMACD) <- c("key","date", "symbol","macd", "signal"  )                # Rename tblTrendMACD Table Columns
+colnames(tblTrendTDI) <- c("key","date", "symbol", "tdi", "di" )                      # Rename tblTrendTCI Table Columns
+colnames(tblTrendVHF) <- c("key","date", "symbol", "vhfClose")                        # Rename tblTrendTCI Table Columns
+colnames(tblTrendVHF_HiLo) <- c("key","date", "symbol", "vhfHiLo")                    # Rename tblTrendTCI Table Columns
+colnames(tblTrendWilderSum) <- c("key","date", "symbol", "runWilderSum")              # Rename tblTrendWilderSum Table Columns
+colnames(tblTrendZigZag) <- c("key","date", "symbol", "zigzag")                       # Rename tblTrendZigZag Table Columns
 # Volatility Tables
-colnames(tblVolatilityATR14) <- c("date", "symbol", "tr", "atr", "trueHigh", "trueLow" )  # Rename tblVolatilityATR14 Table Columns
-colnames(tblVolatilityBBands20) <- c("date", "symbol", "dn", "mavg", "up", "pctB", 
+colnames(tblVolatilityATR14) <- c("key","date", "symbol", "tr", "atr", "trueHigh", "trueLow" )  # Rename tblVolatilityATR14 Table Columns
+colnames(tblVolatilityBBands20) <- c("key","date", "symbol", "dn", "mavg", "up", "pctB", 
   "bbDisp", "bbDispDiff")                                                       # Rename BBands Table Columns
-colnames(tblVolatilityChaikin) <- c("date", "symbol", "volatilityChaikin")      # Rename ChaikinVol Table Columns
-colnames(tblVolatilityClose) <- c("date", "symbol", "volatilityClose")          # Rename tblVolatility_Close Table Columns
-colnames(tblVolatilityPBands) <- c("date", "symbol", "dn", "center", "up" )     # Rename tblVolatilityPBands Table Columns
+colnames(tblVolatilityChaikin) <- c("key","date", "symbol", "volatilityChaikin")      # Rename ChaikinVol Table Columns
+colnames(tblVolatilityClose) <- c("key","date", "symbol", "volatilityClose")          # Rename tblVolatility_Close Table Columns
+colnames(tblVolatilityPBands) <- c("key","date", "symbol", "dn", "center", "up" )     # Rename tblVolatilityPBands Table Columns
 # Volume Tables
-colnames(tblVolumeChaikinAD) <- c("date", "symbol", "ad")                       # Rename ChaikinAD Table Columns
-colnames(tblVolumeChaikinMF) <- c("date", "symbol", "cmf")                      # Rename ChaikinMF Table Columns
-colnames(tblVolumeOBV) <- c("date", "symbol", "obv")                            # Rename OBV Table Columns
+colnames(tblVolumeChaikinAD) <- c("key","date", "symbol", "ad")                       # Rename ChaikinAD Table Columns
+colnames(tblVolumeChaikinMF) <- c("key","date", "symbol", "cmf")                      # Rename ChaikinMF Table Columns
+colnames(tblVolumeOBV) <- c("key","date", "symbol", "obv")                            # Rename OBV Table Columns
 
 # Moving Average Tables
-colnames(tblMA_ALMA20) <- c("symbol", "alma20")                                 # Rename Arnaud Legoux moving average columns
-colnames(tblMA_DEMA20) <- c("date", "symbol", "dema20")                         # Rename Double Exponential Moving Average columns
-colnames(tblMA_EMA20) <- c("date", "symbol", "ema20")                           # Create Exponential Moving Average table
-colnames(tblMA_EVWMA20) <- c("date", "symbol", "evma20")                        # Create # Elastic Volume-weighted Moving Average table
-colnames(tblMA_HMA20) <- c("date", "symbol", "hma20")                           # Create Hull moving average table
-colnames(tblMA_SMA20) <- c("date", "symbol", "sma20")                           # Create Simple Moving Average table
+colnames(tblMA_ALMA20) <- c("symbol", "alma20")                                                     # Rename Arnaud Legoux moving average columns
+colnames(tblMA_DEMA20) <- c("key","date", "symbol", "dema20")                                       # Rename Double Exponential Moving Average columns
+colnames(tblMA_EMA20) <- c("key","date", "symbol", "ema20")                                         # Create Exponential Moving Average table
+colnames(tblMA_EVWMA20) <- c("key","date", "symbol", "evma20")                                      # Create # Elastic Volume-weighted Moving Average table
+colnames(tblMA_HMA20) <- c("key","date", "symbol", "hma20")                                         # Create Hull moving average table
+colnames(tblMA_SMA20) <- c("key","date", "symbol", "sma20")                                         # Create Simple Moving Average table
 #vma <- 
-colnames(tblMA_TRIX) <- c("date", "symbol", "trix", "signal")                   # Create Triple Smoothed Exponential Oscillator table
-colnames(tblMA_VWAS20) <- c("date", "symbol", "vwas20")                         # Create Volume-weighted moving average table
-colnames(tblMA_WMA20) <- c("date", "symbol", "wma20")                           # Create # Weighted Moving Average table
-colnames(tblMA_ZLEMA20) <- c("date", "symbol", "zlema20")                       # Rename ZLEMA Table Columns
-# Indexing  --------------------------------------------------------------------
-tblPrice <- tibble::rowid_to_column(tblPrice, "Index")                          # Add Index to tblPrice
-# Reorder  ---------------------------------------------------------------------
-tblPrice <- tblPrice[, c(1, 10, 2, 9, 8, 3, 4, 5, 6, 7)]                        # Reorder tblPrice columns
+colnames(tblMA_TRIX) <- c("key","date", "symbol", "trix", "signal")                                 # Create Triple Smoothed Exponential Oscillator table
+colnames(tblMA_VWAS20) <- c("key","date", "symbol", "vwas20")                                       # Create Volume-weighted moving average table
+colnames(tblMA_WMA20) <- c("key","date", "symbol", "wma20")                                         # Create # Weighted Moving Average table
+colnames(tblMA_ZLEMA20) <- c("key","date", "symbol", "zlema20")                                     # Rename ZLEMA Table Columns
+# Indexing  ----------------------------------------------------------------------------------------
+tblPrice <- tibble::rowid_to_column(tblPrice, "Index")                                              # Add Index to tblPrice
+# Reorder  -----------------------------------------------------------------------------------------
+tblPrice <- tblPrice[, c(2, 1, 3, 11, 10, 4, 5, 6, 7, 9, 8)]                                            # Reorder tblPrice columns
 
-# Output -----------------------------------------------------------------------
+# Output -------------------------------------------------------------------------------------------
 # tblEMV
 # tblMOM
 # tblMOM_CCI
